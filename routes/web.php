0a0b729c6cd1d41531cf2001bb1f93c87b80a701
WladimirOSZ <cg3014428@LAB117M09.IFSP>
2022-08-25 21:35:02 -0300

    tela de cadastro

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\{LoginController,RegisterController};
 use App\Http\Controllers\Dashboard\DashboardController;
-
-
 
 
 
 Route::get('/', [LoginController::class , 'create'])->name('auth.login.create');
 Route::post('/', [LoginController::class , 'store'])->name('auth.login.store');
-Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+Route::get('/cadastro_de_usuario', [RegisterController::class , 'create'])->name('auth.register.create');
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
