0ba34d0c1989edf43dae9f3aa9c3b1d6817bb377
WladimirOSZ <cg3014428@LAB117M09.IFSP>
2022-09-08 22:06:38 -0300

    Progresso

--- resources/views/exams/store.blade.php
+++ resources/views/exams/store.blade.php
@@ -9,8 +9,11 @@
         #visualizacaoProva{
             width: 800px;
         }
+        .answers{
+            list-style-type: none;
+            padding-left: 10px;
+        }
     </style>
-
 @endsection
 
 @section('content')
@@ -28,7 +31,7 @@
                 </div>
                 <div class="row mb-3 d-flex justify-content-center" >
                     <div class="card border border-secondary h-100 py-2">
-                        <div class="card-body text-dark" id="visualizacaoProva">
+                        <div class="card-body text-dark " id="visualizacaoProva">
                             <div class="testHeader">
                                 <img src="{{asset('img/logocaraguasecretaria.png')}}"
                                 class="d-inline"
@@ -36,13 +39,179 @@
                                 <h5 class="d-inline">CEI/EMEI Prof° Aparecida Maria Pires de Meneses</h5>
                             </div>
                             {{-- quebra de opções --}}
-                            <div class="headerInfo d-flex justify-content-center ">
+                            <div class="headerInfo d-flex flex-column align-items-center">
                                 <h3>{{$formData['name']}}</h3>
+                                <h6 class="m-0">Professor {{Auth::user()->name}}</h6>
+                                <h6 class="m-0 ">Caraguatatuba, 25 de maio de 2022</h6>
                             </div>
-                            <h5 class="m-0">Professor {{Auth::user()->name}}</h5>
-                            <h5 class="m-0 ">Caraguatatuba, 25 de maio de 2022</h5>
+
+
+
+
+                            <div class="questions">
+
+                                <h6 class="font-weight-bold mt-3">Questão 1</h6>
+                                <p>Por mais bem informado que você possa ser, não dá para
+                                    baixar a guarda. Mas por que as notícias falsas – mesmo
+                                    aquelas mais improváveis – parecem tão convincentes para
+                                    tantas pessoas? Van Bavel, professor de psicologia e ciência
+                                    neural da Universidade de Nova York, se especializou em
+                                    entender como as crenças políticas e identidades de grupo
+                                    influenciam a mais bem informado que você possa ser, não dá para
+                                    baixar a guarda. Mas por que as notícias falsas – mesmo
+                                    aquelas mais improváveis – parecem tão convincentes para
+                                    tantas pessoas? Van Bavel, professor de psicologia e ciência
+                                    neural da Universidade de Nova York, se especializou em
+                                    entender como as crenças políticas e identidades de grupo
+                                    influenciam a mente, e descobriu que a identificação com
+                                    posições políticas pode interferir em como o cérebro processa
+                                    as informações.  mais bem informado que você possa ser, não dá para
+                                    baixar a guarda. Mas por que as notícias falsas – mesmo
+                                    aquelas mais improváveis – parecem tão convincentes para
+                                    tantas pessoas? Van Bavel, professor de psicologia e ciência
+                                    neural da Universidade de Nova York, se especializou em
+                                    entender como as crenças políticas e identidades de grupo
+                                    influenciam a mente, e descobriu que a identificação com
+                                    posições políticas pode interferir em como o cérebro processa
+                                    as informações.  mais bem informado que você possa ser, não dá para
+                                    baixar a guarda. Mas por que as notícias falsas – mesmo
+                                    aquelas mais improváveis – parecem tão convincentes para
+                                    tantas pessoas? Van Bavel, professor de psicologia e ciência
+                                    neural da Universidade de Nova York, se especializou em
+                                    entender como as crenças políticas e identidades de grupo
+                                    influenciam a mente, e descobriu que a identificação com
+                                    posições políticas pode interferir em como o cérebro processa
+                                    as informações.  mais bem informado que você possa ser, não dá para
+                                    baixar a guarda. Mas por que as notícias falsas – mesmo
+                                    aquelas mais improváveis – parecem tão convincentes para
+                                    tantas pessoas? Van Bavel, professor de psicologia e ciência
+                                    neural da Universidade de Nova York, se especializou em
+                                    entender como as crenças políticas e identidades de grupo
+                                    influenciam a mente, e descobriu que a identificação com
+                                    posições políticas pode interferir em como o cérebro processa
+                                    as informações.  mais bem informado que você possa ser, não dá para
+                                    baixar a guarda. Mas por que as notícias falsas – mesmo
+                                    aquelas mais improváveis – parecem tão convincentes para
+                                    tantas pessoas? Van Bavel, professor de psicologia e ciência
+                                    neural da Universidade de Nova York, se especializou em
+                                    entender como as crenças políticas e identidades de grupo
+                                    influenciam a mente, e descobriu que a identificação com
+                                    posições políticas pode interferir em como o cérebro processa
+                                    as informações.  mais bem informado que você possa ser, não dá para
+                                    baixar a guarda. Mas por que as notícias falsas – mesmo
+                                    aquelas mais improváveis – parecem tão convincentes para
+                                    tantas pessoas? Van Bavel, professor de psicologia e ciência
+                                    neural da Universidade de Nova York, se especializou em
+                                    entender como as crenças políticas e identidades de grupo
+                                    influenciam a mente, e descobriu que a identificação com
+                                    posições políticas pode interferir em como o cérebro processa
+                                    as informações. mente, e descobriu que a identificação com
+                                    posições políticas pode interferir em como o cérebro processa
+                                    as informações.
+                                    Tendemos a rejeitar fatos que ameaçam nosso senso de
+                                    identidade e sempre buscar informações que confirmem
+                                    nossas próprias crenças, seja por meio de memórias seletivas,
+                                    leituras de fontes que estão do nosso lado ou mesmo
+                                    interpretando os fatos de determinada maneira. Isso tudo
+                                    está relacionado a não querermos ter nossas ideias, gostos,
+                                    identidade questionados, e por isso temos dificuldade em
+                                    aceitar o que contradiz aquilo em que acreditamos. De acordo com o texto:</p>
+                                {{-- passa pra um banco de questoes imaginario,
+                                    busca de la e muda isso tudo pra foereach --}}
+                                <ul class="answers">
+                                    <li>
+                                        <span>A)</span>
+                                        as pessoas bem informadas estão protegidas de
+                                        polêmicas, uma vez que o modo como processam os fatos
+                                        tornam suas opiniões mais convincentes.
+                                    </li>
+                                    <li>
+                                        <span>B)</span>
+                                        o posicionamento político garante a disseminação de
+                                        notícias verdadeiras e resulta de crenças já estabelecidas
+                                        socialmente.
+                                    </li>
+                                    <li>
+                                        <span>C)</span>
+                                        a rejeição a questionamentos impede que se admitam
+                                        pontos de vista antagônicos, em razão da tendência de se
+                                        confirmar crenças pessoais.
+                                    </li>
+                                    <li>
+                                        <span>D)</span>
+                                        as memórias seletivas auxiliam na rejeição de fatos
+                                        contrários à realidade, já que conduzem à interpretação
+                                        das reportagens de modo imparcial.
+                                    </li>
+                                    <li>
+                                        <span>E)</span>
+                                        os fatos hostis normalmente são preteridos por grande
+                                        parte da sociedade, embora sejam utilizados como
+                                        identificador cultural.
+                                    </li>
+                                </ul>
+
+                                <h6 class="font-weight-bold mt-3">Questão 2</h6>
+                                <p>Por mais bem informado que você possa ser, não dá para
+                                    baixar a guarda. Mas por que as notícias falsas – mesmo
+                                    aquelas mais improváveis – parecem tão convincentes para
+                                    tantas pessoas? Van Bavel, professor de psicologia e ciência
+                                    neural da Universidade de Nova York, se especializou em
+                                    entender como as crenças políticas e identidades de grupo
+                                    influenciam a mente, e descobriu que a identificação com
+                                    posições políticas pode interferir em como o cérebro processa
+                                    as informações.
+                                    Tendemos a rejeitar fatos que ameaçam nosso senso de
+                                    identidade e sempre buscar informações que confirmem
+                                    nossas próprias crenças, seja por meio de memórias seletivas,
+                                    leituras de fontes que estão do nosso lado ou mesmo
+                                    interpretando os fatos de determinada maneira. Isso tudo
+                                    está relacionado a não querermos ter nossas ideias, gostos,
+                                    identidade questionados, e por isso temos dificuldade em
+                                    aceitar o que contradiz aquilo em que acreditamos. De acordo com o texto:</p>
+                                {{-- passa pra um banco de questoes imaginario,
+                                    busca de la e muda isso tudo pra foereach --}}
+                                <ul class="answers">
+                                    <li>
+                                        <span>A)</span>
+                                        as pessoas bem informadas estão protegidas de
+                                        polêmicas, uma vez que o modo como processam os fatos
+                                        tornam suas opiniões mais convincentes.
+                                    </li>
+                                    <li>
+                                        <span>B)</span>
+                                        o posicionamento político garante a disseminação de
+                                        notícias verdadeiras e resulta de crenças já estabelecidas
+                                        socialmente.
+                                    </li>
+                                    <li>
+                                        <span>C)</span>
+                                        a rejeição a questionamentos impede que se admitam
+                                        pontos de vista antagônicos, em razão da tendência de se
+                                        confirmar crenças pessoais.
+                                    </li>
+                                    <li>
+                                        <span>D)</span>
+                                        as memórias seletivas auxiliam na rejeição de fatos
+                                        contrários à realidade, já que conduzem à interpretação
+                                        das reportagens de modo imparcial.
+                                    </li>
+                                    <li>
+                                        <span>E)</span>
+                                        os fatos hostis normalmente são preteridos por grande
+                                        parte da sociedade, embora sejam utilizados como
+                                        identificador cultural.
+                                    </li>
+                                </ul>
+
+                            </div>
+
+
+
+
 
                         </div>
+
                     </div>
                 </div>
             </div>
